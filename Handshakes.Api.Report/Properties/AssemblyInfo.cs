3a0ad23cacc83a1e764cb6c43e91e242fbff46a1
Vee Jirapongcharoenlarb <vee.jirapong@gmail.com>
2018-03-19 15:19:47 +0700

    Updated to support nuget

--- Handshakes.Api.Report/Properties/AssemblyInfo.cs
+++ Handshakes.Api.Report/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Handshakes.Api.Report")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Report Generator based on OpenXML")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MyCos Tech")]
 [assembly: AssemblyProduct("Handshakes.Api.Report")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Set Inernal class visible for Unit Testing
