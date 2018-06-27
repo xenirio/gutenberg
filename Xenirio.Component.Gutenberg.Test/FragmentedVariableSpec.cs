ff8422e839cdd9079af7823233537c68ed4aeabe
Vee Jirapongcharoenlarb <vee.jirapong@gmail.com>
2018-06-27 13:50:58 +0700

    Update test spec to verify merged field code

--- Xenirio.Component.Gutenberg.Test/FragmentedVariableSpec.cs
+++ Xenirio.Component.Gutenberg.Test/FragmentedVariableSpec.cs
@@ -10,117 +10,17 @@ namespace Xenirio.Component.Gutenberg.Test
     public class FragmentedVariableSpec
     {
         [TestMethod]
-        public void Incorrect_VariableName_ThrowsArgumentOut()
+        public void Should_Merge_Fragmented_Variable_Of_Page_Break_Table()
         {
-            try
-            {
-                var sourcefile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariable.docx";
-                var outfile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariableTest.docx";
-                if (File.Exists(outfile))
-                    File.Delete(outfile);
-                File.Copy(sourcefile, outfile);
-                var document = new ReportDocument();
-
-                document.InjectReportElement(new ReportLabel() { Key = "Entity.Names", Value = "IPO of Ezra Holdings Limited" });
-
-                document.Save(outfile);
-            }
-            catch (Exception e)
-            {
-                Assert.AreEqual("Variables not match with Fields", e.Message);
-            }
+            var sourcefile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariable.docx";
+            var outfile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariableTest.docx";
+            if (File.Exists(outfile))
+                File.Delete(outfile);
+            File.Copy(sourcefile, outfile);
+            var document = new ReportDocument();
+            document.InjectReportElement(new ReportLabel() { Key = "Entity.Name", Value = "IPO of Ezra Holdings Limited & Coopers" });
+            document.Save(outfile);
         }
-
-        [TestMethod]
-        public void Incorrect_VariableType_ThrowsArgumentOut()
-        {
-            try
-            {
-                var sourcefile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariable.docx";
-                var outfile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariableTest.docx";
-                if (File.Exists(outfile))
-                    File.Delete(outfile);
-                File.Copy(sourcefile, outfile);
-                var document = new ReportDocument();
-
-                document.InjectReportElement(new ReportTable()
-                {
-                    Key = "Entity.Name",
-                    Elements = new ReportLabel[][] {
-                        new ReportLabel[] {
-                            new ReportLabel() { Value = "Coopers" },
-                            new ReportLabel() { Value = "Firm" },
-                            new ReportLabel() { Value = "Chiang Mai" },
-                            new ReportLabel() { Value = "John" },
-                            new ReportLabel() { Value = "Good" },
-                            new ReportLabel() { Value = "Chiang Mai" },
-                            new ReportLabel() { Value = "John" },
-                            new ReportLabel() { Value = "Good" },
-                        }
-                    }
-                });
-                document.Save(outfile);
-            }
-            catch(Exception e)
-            {
-                Assert.AreEqual("Specified argument was out of the range of valid values.\r\nParameter name: index", e.Message);
-            }
-           
-        }
-
-        [TestMethod]
-        public void Duplicate_VariableName_ThrowArgumentOut()
-        {
-            try
-            {
-                var sourcefile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariable.docx";
-                var outfile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariableTest.docx";
-                if (File.Exists(outfile))
-                    File.Delete(outfile);
-                File.Copy(sourcefile, outfile);
-                var document = new ReportDocument();
-
-                document.InjectReportElement(new ReportLabel() { Key = "Entity.Name", Value = "IPO of Ezra Holdings Limited" });
-                document.InjectReportElement(new ReportLabel() { Key = "Entity.Name", Value = "DBS Banking" });
-
-                document.Save(outfile);
-
-            }
-            catch(Exception e)
-            {
-                Assert.AreEqual("An item with the same key has already been added.", e.Message);
-            }
-
-        }
-
-        [TestMethod]
-        public void Incorrect_VariableSize_ThrowArgumentOut()
-        {
-            try
-            {
-                var sourcefile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariable.docx";
-                var outfile = Environment.CurrentDirectory + @"\Resources\SampleFragmentedVariableTest.docx";
-                if (File.Exists(outfile))
-                    File.Delete(outfile);
-                File.Copy(sourcefile, outfile);
-                var document = new ReportDocument();
-
-                document.InjectReportElement(new ReportLabels() { Key = "Entity.Name", Values = new string[] { "IPO of Ezra Holdings Limited", "IPO of Ezra" } });
-
-
-                document.Save(outfile);
-
-            }
-            catch (Exception e)
-            {
-                Assert.AreEqual("An item with the same key has already been added.", e.Message);
-            }
-
-        }
-
-
-
-
     }
 
 }
