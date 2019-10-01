aac748bfa18fe5352403df84aa718eb9bb2dc3dc
Vee <vee@mycostech.com>
2019-10-01 17:56:41 +0700

    Initial Json extension

---
+++ Xenirio.Component.Gutenberg.Extension/ReportGeneratorExtension.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xenirio.Component.Gutenberg.Extension.Json
+{
+    public static class ReportGeneratorJsonExtension
+    {
+        public static void setJsonToken(this ReportGenerator context, JObject json)
+        {
+            
+        }
+
+        private static Dictionary<string, object> deserializeAndFlatten(string json)
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            JToken token = JToken.Parse(json);
+            fillDictionaryFromJToken(dict, token, "");
+            return dict;
+        }
+
+        private static void fillDictionaryFromJToken(Dictionary<string, object> dict, JToken token, string prefix)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    foreach (JProperty prop in token.Children<JProperty>())
+                    {
+                        fillDictionaryFromJToken(dict, prop.Value, join(prefix, prop.Name));
+                    }
+                    break;
+
+                case JTokenType.Array:
+                    int index = 0;
+                    foreach (JToken value in token.Children())
+                    {
+                        fillDictionaryFromJToken(dict, value, join(prefix, index.ToString()));
+                        index++;
+                    }
+                    break;
+
+                default:
+                    dict.Add(prefix, ((JValue)token).Value);
+                    break;
+            }
+        }
+
+        private static string join(string prefix, string name)
+        {
+            return (string.IsNullOrEmpty(prefix) ? name : prefix + "." + name);
+        }
+    }
+}
